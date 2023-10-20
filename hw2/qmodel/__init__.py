5c1d0364471158dbceb6a818c3adb7c3932e76a5
marvleon <marvleon@pdx.edu>
2023-10-20 12:24:13 -0700

    file description for derived model class and package constructor

---
+++ hw2/qmodel/__init__.py
@@ -0,0 +1,4 @@
+# Marvin Leon cs430
+# This file contains the constructor for the packackage when its imported by other python code.
+# The constructor sets which model backend is going to be used for the site (sqlite3).
+# The constructor instantiates the model from get_model() fucntion to return it to the web app.
--- hw2/qmodel/model_sqlite3.py
+++ hw2/qmodel/model_sqlite3.py
@@ -1,3 +1,10 @@
 # Marvin Leon cs430
-# This file contains the derived data model class that supports creation and reading og
-# entries via a sqlite3 database
+# This file contains the derived data model class that supports creation and reading of entries
+# via a sqlite3 database. This is the backend code.
+
+# The abstract base class Model is imported along with the sqlite3 package.
+# The file storing the database (entries.db) will be created in the directory that the web
+# app is run from and will persist across invocations. 
+
+# The model constructor creates a connection to the file and attempts a query to see
+# if the "quotes" table exists in the file. If not, then it will create the table and schema.
