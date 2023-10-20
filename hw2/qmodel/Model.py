8008c48f31909fc70fb0fc0bb3f996f150b79997
marvleon <marvleon@pdx.edu>
2023-10-20 14:27:05 -0700

    add abstract model class

--- hw2/qmodel/Model.py
+++ hw2/qmodel/Model.py
@@ -1,2 +1,9 @@
 # Marvin Leon cs430
 # This file contains the abstract model class
+
+class Model():
+    def select(self):
+        # Gets al entries from DB return tuple containing all rows of database
+        pass
+    def insert(self, author, quote):
+        pass
