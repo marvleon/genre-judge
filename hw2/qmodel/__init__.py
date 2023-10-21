b856e6b75575f4cfdba4650d3dd3d870e5b7deee
marvleon <marvleon@pdx.edu>
2023-10-20 18:30:32 -0700

    implement constructor for package

--- hw2/qmodel/__init__.py
+++ hw2/qmodel/__init__.py
@@ -2,3 +2,13 @@
 # This file contains the constructor for the packackage when its imported by other python code.
 # The constructor sets which model backend is going to be used for the site (sqlite3).
 # The constructor instantiates the model from get_model() fucntion to return it to the web app.
+
+model_backend = 'sqlite3'
+
+if model_backend == 'sqlite3':
+    from .model_sqlite3 import model
+
+appmodel = model()
+
+def get_model():
+    return appmodel
