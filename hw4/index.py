0ee0249cc0b1e2cb4019ba3ba05b597c91ddf265
marvleon <marvleon@pdx.edu>
2023-11-10 15:23:45 -0800

    add quote implementation and requirements

---
+++ hw4/index.py
@@ -0,0 +1,17 @@
+# Marvin Leon cs430
+# MVP view component that handles the GET request for the home page
+# Renders the homepage with quotes
+
+from flask import render_template
+from flask.views import MethodView
+import qmodel
+
+
+class Index(MethodView):
+    def get(self):
+        model = qmodel.get_model()
+        entries = [
+            dict(quote=row[0], author=row[1], source=row[2], date=row[3])
+            for row in model.select()
+        ]
+        return render_template("index.html", entries=entries)
