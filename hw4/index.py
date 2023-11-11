4fab9455b269a8d0ee1448e24faaaa3d0f23e6b5
marvleon <marvleon@pdx.edu>
2023-11-10 17:41:04 -0800

    update to include new 'added time' entry

--- hw4/index.py
+++ hw4/index.py
@@ -11,7 +11,7 @@ class Index(MethodView):
     def get(self):
         model = qmodel.get_model()
         entries = [
-            dict(quote=row[0], author=row[1], source=row[2], date=row[3])
+            dict(quote=row[0], author=row[1], source=row[2], date=row[3], added=row[4])
             for row in model.select()
         ]
         return render_template("index.html", entries=entries)
