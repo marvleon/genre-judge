5a9a0aaa545dc9d2f8386062ed301d94ff0094fb
marvleon <marvleon@pdx.edu>
2023-11-10 16:28:09 -0800

    implement datastore derived backend

---
+++ hw4/qmodel/model_datastore.py
@@ -0,0 +1,42 @@
+
+from .Model import Model
+from datetime import datetime
+from google.cloud import datastore
+
+def from_datastore(entity):
+    """
+    Datastore returns:
+        [Entity{key: (kind, id), prop: val, ...}]
+
+    This returns:
+        [ quote, author, source, date, added ]
+    'quote', 'author', 'source', and 'date' are Python strings
+    and 'added' is a Python datetime
+    """
+    if not entity:
+        return None
+    if isinstance(entity, list):
+        entity = entity.pop()
+    return [entity['quote'],entity['author'],entity['source'],entity['date'],entity['added']]
+
+class model(Model):
+    def __init__(self):
+        self.client = datastore.Client('cloud-leon-marvleon')
+
+    def select(self):
+        query = self.client.query(kind = 'Quote')
+        entities = list(map(from_datastore,query.fetch()))
+        return entities
+
+    def insert(self,name,email,message):
+        key = self.client.key('Quote')
+        rev = datastore.Entity(key)
+        rev.update( {
+            'quote': quote,
+            'author' : author,
+            'source': source,
+            'date' : date,
+            'added' : datetime.today()
+            })
+        self.client.put(rev)
+        return True
