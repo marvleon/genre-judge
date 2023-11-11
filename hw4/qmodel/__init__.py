12e172d8035e03fb4210c46a4d000f7bac6c9c4c
marvleon <marvleon@pdx.edu>
2023-11-10 17:54:50 -0800

    update constructor and remove old sqlite3 file

--- hw4/qmodel/__init__.py
+++ hw4/qmodel/__init__.py
@@ -5,13 +5,10 @@
 
 model_backend = "datastore"
 
-if model_backend == "sqlite3":
-    from .model_sqlite3 import model
-elif model_backend == "datastore":
+if model_backend == "datastore":
     from .model_datastore import model
 
 appmodel = model()
-
 
 def get_model():
     return appmodel
