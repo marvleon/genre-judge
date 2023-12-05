1524b55c1dc57e6865f5a9c0d3eb8e8d687bad42
marvleon <marvleon@pdx.edu>
2023-12-05 12:56:33 -0800

    rename class

--- final/tracks.py
+++ final/tracks.py
@@ -6,7 +6,7 @@ from flask import render_template, redirect, url_for
 from your_module import get_token 
 import spotipy
 
-class GetTracksView(MethodView):
+class GetTracks(MethodView):
     def get(self):
         try: 
             token_info = get_token()
