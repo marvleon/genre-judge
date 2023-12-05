5510501860e0f5831c4575d5d2673d8a8242ea34
marvleon <marvleon@pdx.edu>
2023-12-05 13:04:58 -0800

    create separate file for Oauth management

---
+++ final/util.py
@@ -0,0 +1,32 @@
+#Marvin Leon cs430
+#This file contains the shared utility functions that are used
+#by the different method views.
+from flask import url_for, session
+from spotipy.oauth2 import SpotifyOAuth
+from credentials import CLIENT_ID, CLIENT_SECRET
+import time
+
+TOKEN_CODE = "token_info"
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=url_for("redirectPage", _external=True),
+        scope="user-top-read user-library-read"
+    )
+
+def get_token():
+    token_info = session.get(TOKEN_CODE, None)
+    if not token_info:
+        raise Exception("No token info available in session.")
+    
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    
+    if is_expired:
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        session[TOKEN_CODE] = token_info
+    
+    return token_info
