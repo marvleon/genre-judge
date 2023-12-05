35abe42bc35545958ca47fa3d65ef00853824c3e
marvleon <marvleon@pdx.edu>
2023-12-05 12:46:56 -0800

    remove previous 'Quote' implementation

--- final/index.py
+++ final/index.py
@@ -1,18 +1,13 @@
 # Marvin Leon cs430
 # MVP view component that handles the GET request for the home page
-# Renders the homepage with quotes
+# Renders the homepage
 
 from flask import render_template
-from flask.views import MethodView
-import qmodel
+from flask.views import render_template
 
 #This class  is a Flask view for the index page
 class Index(MethodView):
     #Retrieves all entries from the model and renders them using index.html 
     def get(self):
-        model = qmodel.get_model()
-        entries = [
-            dict(quote=row[0], author=row[1], source=row[2], date=row[3], added=row[4])
-            for row in model.select()
-        ]
-        return render_template("index.html", entries=entries)
+        name = 'username'
+        return render_template("index.html", title='Welcome', username=name)
