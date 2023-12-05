ea085c128ab450a09ea36daa02a45c9caca82456
marvleon <marvleon@pdx.edu>
2023-12-05 12:56:07 -0800

    add getter for track data

---
+++ final/tracks.py
@@ -0,0 +1,17 @@
+# Marvin Leon cs430
+# This class manages the music track data functionality
+
+from flask.views import MethodView
+from flask import render_template, redirect, url_for
+from your_module import get_token 
+import spotipy
+
+class GetTracksView(MethodView):
+    def get(self):
+        try: 
+            token_info = get_token()
+        except: 
+            print("user not logged in")
+            return redirect("/")
+        sp = spotipy.Spotify(auth=token_info['access_token'])
+        # ... rest of your code ...
