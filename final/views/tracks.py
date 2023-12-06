9cf72deb86ac7115607d2c4b4e52e26a4b6c52db
marvleon <marvleon@pdx.edu>
2023-12-05 17:54:06 -0800

    fix time_range

--- final/views/tracks.py
+++ final/views/tracks.py
@@ -3,7 +3,7 @@
 
 from flask.views import MethodView
 from flask import render_template, redirect
-from util import get_token
+import util
 from time import gmtime
 import spotipy
 import os
@@ -11,7 +11,7 @@ import os
 class GetTracks(MethodView):
     def get(self):
         try: 
-            token_info = get_token()
+            token_info = util.get_token()
         except: 
             print("user not logged in")
             return redirect("/")
@@ -20,17 +20,17 @@ class GetTracks(MethodView):
         short_term = sp.current_user_top_tracks(
             limit=10,
             offset=0,
-            time_range=SHORT_TERM,
+            time_range=util.SHORT_TERM,
         )
         medium_term = sp.current_user_top_tracks(
             limit=10,
             offset=0,
-            time_range=MEDIUM_TERM,
+            time_range=util.MEDIUM_TERM,
         )
         long_term = sp.current_user_top_tracks(
             limit=10,
             offset=0,
-            time_range=LONG_TERM,
+            time_range=util.LONG_TERM,
         )
 
         if os.path.exists(".cache"): 
