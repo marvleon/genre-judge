d908b8b0e27bfebbb6b7e525c3bcbb99f1a6af83
marvleon <marvleon@pdx.edu>
2023-12-05 17:35:25 -0800

    move track functionality to util.py

--- final/views/tracks.py
+++ final/views/tracks.py
@@ -2,8 +2,8 @@
 # This class manages the music track data functionality
 
 from flask.views import MethodView
-from flask import render_template, redirect, url_for
-from util import create_spotify_oauth, get_token
+from flask import render_template, redirect
+from util import get_token
 import spotipy
 
 class GetTracks(MethodView):
@@ -16,21 +16,6 @@ class GetTracks(MethodView):
         sp = spotipy.Spotify(auth=token_info['access_token'])
         
         current_user_name = sp.current_user()['display_name']
-        short_term = sp.current_user_top_tracks(
-            limit=10,
-            offset=0,
-            time_range=SHORT_TERM,
-        )
-        medium_term = sp.current_user_top_tracks(
-            limit=10,
-            offset=0,
-            time_range=MEDIUM_TERM,
-        )
-        long_term = sp.current_user_top_tracks(
-            limit=10,
-            offset=0,
-            time_range=LONG_TERM,
-        )
 
         if os.path.exists(".cache"): 
             os.remove(".cache")
