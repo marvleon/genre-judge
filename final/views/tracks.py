e939e35bd402518ce6a59d09d31a9a0edf9df1f8
marvleon <marvleon@pdx.edu>
2023-12-05 17:50:54 -0800

    update getter to retrieve tracks from API

--- final/views/tracks.py
+++ final/views/tracks.py
@@ -4,6 +4,7 @@
 from flask.views import MethodView
 from flask import render_template, redirect
 from util import get_token
+from time import gmtime
 import spotipy
 import os
 
@@ -16,6 +17,21 @@ class GetTracks(MethodView):
             return redirect("/")
         sp = spotipy.Spotify(auth=token_info['access_token'])
         current_user_name = sp.current_user()['display_name']
+        short_term = sp.current_user_top_tracks(
+            limit=10,
+            offset=0,
+            time_range=SHORT_TERM,
+        )
+        medium_term = sp.current_user_top_tracks(
+            limit=10,
+            offset=0,
+            time_range=MEDIUM_TERM,
+        )
+        long_term = sp.current_user_top_tracks(
+            limit=10,
+            offset=0,
+            time_range=LONG_TERM,
+        )
 
         if os.path.exists(".cache"): 
             os.remove(".cache")
